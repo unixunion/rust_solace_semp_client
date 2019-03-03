a5ef639f60c6e44f052e8e345270c764064522b5
Kegan Holtzhausen <kegan.holtzhausen@kindredgroup.com>
2019-03-03 13:22:45 +0100

    9.0.0.17 backport

--- src/models/msg_vpn_rest_delivery_point_rest_consumer_links.rs
+++ src/models/msg_vpn_rest_delivery_point_rest_consumer_links.rs
@@ -14,7 +14,7 @@ use serde_json::Value;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MsgVpnRestDeliveryPointRestConsumerLinks {
-  /// The URI of this MsgVpnRestDeliveryPointRestConsumer's tlsTrustedCommonNames collection. Available since 2.7.
+  /// The URI of this MsgVpnRestDeliveryPointRestConsumer's tlsTrustedCommonNames collection.
   #[serde(rename = "tlsTrustedCommonNamesUri")]
   tls_trusted_common_names_uri: Option<String>,
   /// The URI of this MsgVpnRestDeliveryPointRestConsumer object.
